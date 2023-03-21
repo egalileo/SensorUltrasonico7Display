8de4ae9b038a8e4c925cb42858d59cbdcd14fdd0
Gali Padilla <erick.galileo@outlook.com>
2023-03-21 11:07:35 -0600

    espanol

--- ejercicio1/ejercicio1.ino
+++ ejercicio1/ejercicio1.ino
@@ -1,101 +1,98 @@
-#define LA1 2
-#define LB1 3
-#define LC1 4
-#define LD1 5
-#define LE1 6
-#define LF1 7
-#define LG1 8
-#define DP1 9
+#define DA1 1
+#define DB1 2
+#define DC1 3
+#define DD1 4
+#define DE1 5
+#define DF1 6 
+#define DG1 7
 
-#define LA2 A0
-#define LB2 A1
-#define LC2 A2
-#define LD2 A3
-#define LE2 A4
-#define LF2 10
-#define LG2 11
+#define DA2 8
+#define DB2 9
+#define DC2 10
+#define DD2 11
+#define DE2 12
+#define DF2 13
+#define DG2 A0
 
-#define ECHO 12
-#define TRIG 13
+#define DP A1
+#define TRIG A2
+#define ECHO A3
 
-byte integerPines[] = {LA1,LB1,LC1,LD1,LE1,LF1,LG1};
-byte decimalPines[] = {LA2,LB2,LC2,LD2,LE2,LF2,LG2};
 
-byte matrix[10][7] = {
+byte pinesUnidades[] = {1,2,3,4,5,6,7};
+byte pinesDecenas[] = {8,9,10,11,12,13,A0};
+
+byte matriz[10][7] = {
   {1,1,1,1,1,1,0}, //cero
   {0,1,1,0,0,0,0}, //uno
   {1,1,0,1,1,0,1}, //dos
   {1,1,1,1,0,0,1}, //tres
   {0,1,1,0,0,1,1}, //cuatro
   {1,0,1,1,0,1,1}, //cinco
-  {1,0,1,1,1,1,1}, //seis
+  {1,0,1,1,1,1,1}, //seis  
   {1,1,1,0,0,0,0}, //siete
   {1,1,1,1,1,1,1}, //ocho
-  {1,1,1,0,0,1,1} //nueve 
+  {1,1,1,1,0,1,1} //nueve
 };
 
 void setup()
 {
-  for(int i = 2; i <= 11; i++){
-    pinMode(i, OUTPUT);
+  
+  for( byte x=0; x<sizeof(pinesUnidades); x++){
+  	pinMode(pinesUnidades[x], OUTPUT);
+    pinMode(pinesDecenas[x], OUTPUT);
   }
-  pinMode(LA2, OUTPUT);
-  pinMode(LB2, OUTPUT);
-  pinMode(LC2, OUTPUT);
-  pinMode(LD2, OUTPUT);
-  pinMode(LE2, OUTPUT);
-  pinMode(LF2, OUTPUT);
-  pinMode(LG2, OUTPUT);
+  pinMode(DP, OUTPUT);
   pinMode(ECHO, INPUT);
   pinMode(TRIG, OUTPUT);
-  
 }
 
-void loop()
-{
-  digitalWrite(DP1,HIGH);
-  int distance = medirCM();
-  showIntegers(getIntegers(distance));
-  showDecimal(getDecimals(distance));
+void loop(){
+  digitalWrite(DP,HIGH);
+  int distancia = medirCM();
+  mostrarUnidades(getUnidades(distancia));
+  mostrarDecenas(getDecenas(distancia));
   delay(100);
 }
-void showIntegers(int number){
-  byte pin, value;
+
+void mostrarUnidades(int numero){
+  byte pin, valor;
   for(int i = 0; i < 7; i++){
-    pin = integerPines[i];
-    value = matrix[number][i];
-    digitalWrite(pin, value);
+    pin = pinesUnidades[i];
+    valor = matriz[numero][i];
+    digitalWrite(pin, valor);
   }
 }
 
-void showDecimal(int number){
-  byte pin, value;
+void mostrarDecenas(int numero){
+  byte pin, valor;
   for(int i = 0; i < 7; i++){
-    pin = decimalPines[i];
-    value = matrix[number][i];
-    digitalWrite(pin, value);
+    pin = pinesDecenas[i];
+    valor = matriz[numero][i];
+    digitalWrite(pin, valor);
   }
 }
 
 int medirCM(){
-  long distance, time;
+  long distancia, tiempo;
   digitalWrite(TRIG,LOW);
   delayMicroseconds(4);
   digitalWrite(TRIG, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIG, LOW);
   
-  time = pulseIn(ECHO, HIGH);
-  distance = time * 10 / 292 / 2;
-  return (int) distance;
+  tiempo = pulseIn(ECHO, HIGH);
+  distancia = tiempo * 10 / 292 / 2;
+  return (int) distancia;
 }
 
-int getIntegers(int distance){
-  return (int) distance / 100;
+int getDecenas(int distancia){
+  return (int) distancia / 100;
 }
 
-int getDecimals(int distance){
-  float decimales = (float) distance/100;
+int getUnidades(int distancia){
+  float decimales = (float) distancia/100;
   int enteros = (int) decimales;
   return round(((decimales - enteros)*100)/10);
+}
 }
